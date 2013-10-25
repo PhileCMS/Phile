010b74b6a1ad38a0b6ffca4998e4308f3c230c14
james2doyle <james2doyle@gmail.com>
2013-10-25 12:38:02 -0400

    new colors and add Phile references

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+define('ROOT_DIR', realpath(dirname(__FILE__)) .'/');
+define('CONTENT_DIR', ROOT_DIR .'content/');
+define('CONTENT_EXT', '.md');
+define('LIB_DIR', ROOT_DIR .'lib/');
+define('PLUGINS_DIR', ROOT_DIR .'plugins/');
+define('THEMES_DIR', ROOT_DIR .'themes/');
+define('CACHE_DIR', LIB_DIR .'cache/');
+
+require(ROOT_DIR .'vendor/autoload.php');
+require(LIB_DIR .'phile.php');
+$phile = new Phile();
