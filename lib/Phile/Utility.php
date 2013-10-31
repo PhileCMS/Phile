00f30d875383b9ce19bacc409a2ea0a9dbcf4cf2
Frank Nägler <franae@blade.fritz.box>
2013-10-31 23:18:11 +0100

    add some more Utility methods

--- lib/Phile/Utility.php
+++ lib/Phile/Utility.php
@@ -7,126 +7,65 @@ namespace Phile;
  * @author Frank Nägler
  *
  */
-class Registry extends \ArrayObject {
-	/**
-	 * Registry object provides storage for shared objects.
-	 * @var Registry
-	 */
-	private static $_registry = null;
+class Utility {
 
 	/**
-	 * Retrieves the default registry instance.
-	 *
-	 * @return Registry
+	 * @return string the current protocol
 	 */
-	public static function getInstance() {
-		if (self::$_registry === null) {
-			self::init();
+	public static function getProtocol() {
+		$protocol = 'http';
+		if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off'){
+			$protocol = 'https';
+		}
+		return $protocol;
+	}
+
+	/**
+	 * @return string
+	 */
+	public static function getBaseUrl() {
+		if (Registry::isRegistered('Phile_Settings')) {
+			$config = Registry::get('Phile_Settings');
+			if (isset($config['base_url']) && $config['base_url']) {
+				return $config['base_url'];
+			}
 		}
 
-		return self::$_registry;
+		$url = '';
+		$request_url = (isset($_SERVER['REQUEST_URI'])) ? $_SERVER['REQUEST_URI'] : '';
+		$script_url  = (isset($_SERVER['PHP_SELF'])) ? $_SERVER['PHP_SELF'] : '';
+		if($request_url != $script_url) $url = trim(preg_replace('/'. str_replace('/', '\/', str_replace('index.php', '', $script_url)) .'/', '', $request_url, 1), '/');
+
+		$protocol = self::getProtocol();
+		return rtrim(str_replace($url, '', $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']), '/');
 	}
 
 	/**
-	 * Set the default registry instance to a specified instance.
+	 * resolve a file path by replace the mod: prefix
 	 *
-	 * @param Registry $registry An object instance of type Registry,
-	 *   or a subclass.
-	 * @return void
+	 * @param $path
+	 * @return string|null the full filepath or null
 	 */
-	public static function setInstance(Registry $registry) {
-		if (self::$_registry !== null) {
-			throw new Exception('Registry is already initialized');
+	public static function resolveFilePath($path) {
+		// resolve MOD: prefix
+		if (strtoupper(substr($path, 0, 3)) == 'MOD') {
+			$path = str_ireplace('mod:', PLUGINS_DIR, $path);
+			if (file_exists($path)) {
+				return $path;
+			}
+		}
+		// check if file exists
+		if (file_exists($path)) {
+			return $path;
 		}
 
-		self::$_registry = $registry;
+		return null;
 	}
 
-	/**
-	 * Initialize the default registry instance.
-	 *
-	 * @return void
-	 */
-	protected static function init() {
-		self::setInstance(new Registry());
-	}
-
-	/**
-	 * Unset the default registry instance.
-	 * Primarily used in tearDown() in unit tests.
-	 * @returns void
-	 */
-	public static function _unsetInstance() {
-		self::$_registry = null;
-	}
-
-	/**
-	 * getter method, basically same as offsetGet().
-	 *
-	 * This method can be called from an object of type Registry, or it
-	 * can be called statically.  In the latter case, it uses the default
-	 * static instance stored in the class.
-	 *
-	 * @param string $index - get the value associated with $index
-	 * @return mixed
-	 * @throws Exception if no entry is registerd for $index.
-	 */
-	public static function get($index) {
-		$instance = self::getInstance();
-
-		if (!$instance->offsetExists($index)) {
-			throw new Exception("No entry is registered for key '$index'");
+	public static function load($file) {
+		if (file_exists($file)) {
+			return include $file;
 		}
-
-		return $instance->offsetGet($index);
-	}
-
-	/**
-	 * setter method, basically same as offsetSet().
-	 *
-	 * This method can be called from an object of type Registry, or it
-	 * can be called statically.  In the latter case, it uses the default
-	 * static instance stored in the class.
-	 *
-	 * @param string $index The location in the ArrayObject in which to store
-	 *   the value.
-	 * @param mixed $value The object to store in the ArrayObject.
-	 * @return void
-	 */
-	public static function set($index, $value) {
-		$instance = self::getInstance();
-		$instance->offsetSet($index, $value);
-	}
-
-	/**
-	 * Returns TRUE if the $index is a named value in the registry,
-	 * or FALSE if $index was not found in the registry.
-	 *
-	 * @param  string $index
-	 * @return boolean
-	 */
-	public static function isRegistered($index) {
-		if (self::$_registry === null) {
-			return false;
-		}
-		return self::$_registry->offsetExists($index);
-	}
-
-	/**
-	 * @param array $array data array
-	 * @param integer $flags ArrayObject flags
-	 */
-	public function __construct($array = array(), $flags = parent::ARRAY_AS_PROPS) {
-		parent::__construct($array, $flags);
-	}
-
-	/**
-	 * @param string $index
-	 * @returns mixed
-	 *
-	 * Workaround for http://bugs.php.net/bug.php?id=40442 (ZF-960).
-	 */
-	public function offsetExists($index) {
-		return array_key_exists($index, $this);
+		return null;
 	}
 }
