d77b6cf4407c6418d3294fba17ec48563a3b87cf
james2doyle <james2doyle@gmail.com>
2013-11-03 13:51:56 -0500

    cleanup and default sort is desc. add markdown plugin to default plugins

--- default_config.php
+++ default_config.php
@@ -1,29 +1,32 @@
 <?php
 
 // try to figure out the install path
-$config['site_title']       = 'PhileCMS';			            // Site title
-$config['base_url']         = \Phile\Utility::getBaseUrl(); 	// Override base URL (e.g. http://example.com)
-$config['install_path']     = '';                               // if you installed phile in a subfolder, e.g. http://example.com/phile/ set this option to "phile/"
-$config['theme']            = 'default'; 			            // Set the theme (defaults to "default")
-$config['date_format']      = 'jS M Y';		                    // Set the PHP date format
-$config['twig_config'] = array(			// Twig settings
-	'cache' => false,					// To enable Twig caching change this to CACHE_DIR
-	'autoescape' => false,				// Autoescape Twig vars
-	'debug' => false					// Enable Twig debug
-);
-$config['pages_order_by']   = 'title';	        // Order pages by "title" (alpha) or "date"
-$config['pages_order']      = 'asc';	        // Order pages "asc" or "desc"
+$config['site_title'] = 'PhileCMS'; // Site title
+$config['base_url'] = \Phile\Utility::getBaseUrl(); // use the Utility class to guess the base_url
+$config['install_path'] = ''; // if you installed phile in a subfolder, e.g. http://example.com/phile/ set this option to "Phile/"
+$config['theme'] = 'default'; // Set the theme
+$config['date_format'] = 'jS M Y'; // Set the PHP date format
+// Twig settings
+$config['twig_config'] = array(
+	'cache' => false, // To enable Twig caching change this to CACHE_DIR
+	'autoescape' => false, // Autoescape Twig vars
+	'debug' => false // Enable Twig debug
+	);
+$config['pages_order_by'] = 'title'; // Order pages by "title" (alpha) or "date"
+$config['pages_order'] = 'desc'; // Order pages "asc" or "desc"
 
+// figure out the timezone
 $timezone = (ini_get('date.timezone')) ? ini_get('date.timezone') : 'UTC';
-$config['timezone']         = $timezone; 	// The default timezone
+$config['timezone'] = $timezone; // The default timezone
 
 // only extend $config['plugins'] and not overwrite it, because some core plugins
 // will be added to this config option by default. So, use this option in this way:
 // $config['plugins']['myCustomPlugin'] = array('active' => true);
 // also notice, each plugin has its own config namespace.
 // activate plugins
-$config['plugins'] = array();
-
-$config['plugins']['phileDemoPlugin'] = array('active' => true);
+$config['plugins'] = array(
+	'phileDemoPlugin' => array('active' => true),
+	'phileMarkdown' => array('active' => true) // the default parser
+	);
 
 return $config;
