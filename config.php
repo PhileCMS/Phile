3b3103c1bdf8c65e9ac0944be7f57de904a49f0a
James Doyle <james2doyle@gmail.com>
2014-02-20 09:08:07 -0500

    add instruction for encryption key value

--- config.php
+++ config.php
@@ -3,6 +3,8 @@
 // use this config file to overwrite the defaults from default_config.php
 // or to make local config changes.
 $config = array();
+// run the generator.php file or fill this with a long string
+// must not be empty
 $config['encryptionKey'] = '';
 
 // it is important to return the $config array!
