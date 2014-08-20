629c578a752fa90cd8e22cfeead25b4b7e1159b8
Stijn-Flipper <w.stijn@gmail.com>
2014-08-20 23:08:54 +0200

    Revert "load settings if not loaded"
    This reverts commit f4491cd93eff4e41105989fa32d40120848119e5.

--- lib/Phile/Repository/Page.php
+++ lib/Phile/Repository/Page.php
@@ -82,11 +82,9 @@ class Page {
 	 * @throws \Phile\Exception
 	 */
 	public function findAll(array $options = [], $folder = CONTENT_DIR) {
-		if ($this->settings === null) {
-			$this->settings = \Phile\Registry::get('Phile_Settings');
+		if ($this->settings !== null) {
+			$options += $this->settings;
 		}
-		$options += $this->settings;
-
 		// ignore files with a leading '.' in its filename
 		$files = Utility::getFiles($folder, '/^.[^\.]*\\' . CONTENT_EXT . '/');
 		$pages = array();
