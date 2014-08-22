df4be4520589350b0be49f3a5d61d25ee8e0d4b8
Frank Nägler <mail@naegler.net>
2014-08-22 10:11:59 +0200

    added more tests

--- tests/Phile/Model/PageTest.php
+++ tests/Phile/Model/PageTest.php
@@ -3,16 +3,52 @@
 namespace PhileTest\Model;
 
 class PageTest extends \PHPUnit_Framework_TestCase {
+	/**
+	 * @var \Phile\Repository\Page
+	 */
+	protected $pageRepository = null;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->pageRepository = new \Phile\Repository\Page();
+	}
 
 	public function testPageHasMetaInformation() {
-		$repository = new \Phile\Repository\Page();
-		$page = $repository->findByPath('/');
-		$this->assertInstanceOf('\Phile\Model\Meta', $page->getMeta());
+		$this->assertInstanceOf('\Phile\Model\Meta', $this->pageRepository->findByPath('/')->getMeta());
 	}
 
 	public function testPageHasTitle() {
-		$repository = new \Phile\Repository\Page();
-		$page = $repository->findByPath('/');
-		$this->assertEquals('Welcome', $page->getTitle());
+		$this->assertEquals('Welcome', $this->pageRepository->findByPath('/')->getTitle());
+	}
+
+	public function testPageHasContent() {
+		$this->assertGreaterThan(0, strlen($this->pageRepository->findByPath('/')->getContent()));
+	}
+
+	public function testPageHasUrl() {
+		$this->assertGreaterThan(0, strlen($this->pageRepository->findByPath('/')->getUrl()));
+	}
+
+	public function testPageCanSetContent() {
+		$page = $this->pageRepository->findByPath('/');
+		$page->setContent("testContent");
+		$this->assertEquals("<p>testContent</p>\n", $page->getContent());
+	}
+
+	public function testPageHasMetaObject() {
+		$page = $this->pageRepository->findByPath('/');
+		$this->assertInstanceOf('\Phile\Model\Meta', $page->getMeta());
+	}
+
+	public function testPageHasPreviousPage() {
+		$page = $this->pageRepository->findByPath('/sub/page');
+		$this->assertInstanceOf('\Phile\Model\Page', $page->getPreviousPage());
+		$this->assertEquals('Sub Page Index', $page->getPreviousPage()->getTitle());
+	}
+
+	public function testPageHasNextPage() {
+		$page = $this->pageRepository->findByPath('/index');
+		$this->assertInstanceOf('\Phile\Model\Page', $page->getNextPage());
+		$this->assertEquals('Sub Page Index', $page->getNextPage()->getTitle());
 	}
 }
