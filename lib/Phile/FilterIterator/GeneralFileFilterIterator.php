2cf89a234f18ce9a6c56438ee7e0cf6d01581192
Frank Nägler <mail@naegler.net>
2014-08-28 17:22:39 +0200

    return always true, no filter needed here

--- lib/Phile/FilterIterator/GeneralFileFilterIterator.php
+++ lib/Phile/FilterIterator/GeneralFileFilterIterator.php
@@ -7,8 +7,8 @@ class GeneralFileFilterIterator extends \FilterIterator {
 	 * @return bool
 	 */
 	public function accept() {
-		/** @var \SplFileInfo $this */
-		return (preg_match('/^.*/', $this->getFilename()) > 0);
+		// accept all kind of files, no filter
+		return true;
 	}
 
 }
