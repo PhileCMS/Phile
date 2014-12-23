26d86a4ed81a0ac84595f2611bd281e3aebb4f7d
DenBeke <DenBeke@users.noreply.github.com>
2014-12-23 14:57:18 +0100

    [BUGFIX] Nginx: https/http protocol detection
    See pull request #172 of `PhileCMS/Phile`

--- lib/Phile/Core/Utility.php
+++ lib/Phile/Core/Utility.php
@@ -24,7 +24,7 @@ class Utility {
 			return '';
 		}
 		$protocol = 'http';
-		if (isset($_SERVER['HTTPS']) && ( strtolower($_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == '1' ) ) {
+		if (!empty($_SERVER['HTTPS']) && strtolower($_SERVER['HTTPS']) !== 'off' ) {
 			$protocol = 'https';
 		}
 
