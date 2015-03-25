920872750ccebdb4f850e97300a9384287362b0a
Schlaefer <openmail+sourcecode@siezi.com>
2015-03-25 17:32:03 +0100

    install marked for homepage conversion als local npm package

--- converter.sh
+++ converter.sh
@@ -2,8 +2,8 @@
 
 for FILE in Phile.wiki/*.md
   do
-    NAME=$(basename $FILE)
-    marked -o html/${NAME%.*}.html $FILE --gfm
+    NAME=$(basename "$FILE")
+    node_modules/.bin/marked -o html/"${NAME%.*}".html "$FILE" --gfm
 done
 
 exit 0
