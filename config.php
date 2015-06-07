cfa6ad4fe6e4b6c608bba386745ec9647c354d49
Schlaefer <openmail+sourcecode@siezi.com>
2015-06-07 11:57:09 +0200

    cleanup and improve formatting in config files

--- config.php
+++ config.php
@@ -1,11 +1,30 @@
 <?php
 
-// use this config file to overwrite the defaults from default_config.php
-// or to make local config changes.
-$config = array();
-// run the generator.php file or fill this with a long string
-// must not be empty
+/**
+ * Set configuration of your phile installation.
+ *
+ * You can also overwrite Phile-defaults here.
+ */
+$config = [];
+
+/**
+ * encryption key
+ */
 $config['encryptionKey'] = '';
 
-// it is important to return the $config array!
+/**
+ * page title
+ */
+$config['site_title'] = 'PhileCMS';
+
+/**
+ * default theme
+ */
+$config['theme'] = 'default';
+
+/**
+ * demo plugin
+ */
+// $config['plugins']['mycompany\\demoPlugin'] = ['active' => true];
+
 return $config;
