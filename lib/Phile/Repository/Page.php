12908936ff8350dac3955b94c7ff5cd0dbd98aa4
Keith Maika <keithm@aoeex.com>
2015-06-29 22:32:26 -0400

    Use a callback function for loading pages.
    Collection class accepts a callback function which will be executed when
    the pages need to be loaded.  This allows for minimal changes to the
    existing repository class and keeps the search code where it belongs.

--- lib/Phile/Repository/Page.php
+++ lib/Phile/Repository/Page.php
@@ -87,8 +87,62 @@ class Page {
 	 * @return PageCollection of \Phile\Model\Page objects
 	 */
 	public function findAll(array $options = array(), $folder = CONTENT_DIR) {
-		$options += $this->settings;
-        return new PageCollection($options, $folder, $this);
+		return new PageCollection(function() use ($options, $folder){
+			$options += $this->settings;
+			// ignore files with a leading '.' in its filename
+			$files = Utility::getFiles($folder, '\Phile\FilterIterator\ContentFileFilterIterator');
+			$pages = array();
+			foreach ($files as $file) {
+				if (str_replace($folder, '', $file) == '404' . CONTENT_EXT) {
+					// jump to next page if file is the 404 page
+					continue;
+				}
+				$pages[] = $this->getPage($file, $folder);
+			}
+
+			if (empty($options['pages_order'])) {
+				return $pages;
+			}
+
+			// parse search	criteria
+			$terms = preg_split('/\s+/', $options['pages_order'], -1, PREG_SPLIT_NO_EMPTY);
+			foreach ($terms as $term) {
+				$term = explode('.', $term);
+				if (count($term) > 1) {
+					$type = array_shift($term);
+				} else {
+					$type = null;
+				}
+				$term = explode(':', $term[0]);
+				$sorting[] = array('type' => $type, 'key' => $term[0], 'order' => $term[1]);
+			}
+
+			// prepare search criteria for array_multisort
+			foreach ($sorting as $sort) {
+				$key = $sort['key'];
+				$column = array();
+				foreach ($pages as $page) {
+					/** @var \Phile\Model\Page $page */
+					$meta = $page->getMeta();
+					if ($sort['type'] === 'page') {
+						$method = 'get' . ucfirst($key);
+						$value = $page->$method();
+					} elseif ($sort['type'] === 'meta') {
+						$value = $meta->get($key);
+					} else {
+						continue 2; // ignore unhandled search term
+					}
+					$column[] = $value;
+				}
+				$sortHelper[] = $column;
+				$sortHelper[] = constant('SORT_' . strtoupper($sort['order']));
+			}
+			$sortHelper[] = &$pages;
+
+			call_user_func_array('array_multisort', $sortHelper);
+
+			return $pages;
+		});
 	}
 
 	/**
--- lib/Phile/Repository/PageCollection.php
+++ lib/Phile/Repository/PageCollection.php
@@ -2,10 +2,9 @@
 
 namespace Phile\Repository;
 
-use Phile\Core\Utility;
 
 /**
- * Page collection class for locating pages on demand.
+ * Page collection which delays searching for and loading pages until necessary.
  *
  * @author  PhileCMS
  * @link    https://philecms.com
@@ -13,132 +12,53 @@ use Phile\Core\Utility;
  * @package Phile\Repository
  */
 class PageCollection implements \ArrayAccess, \IteratorAggregate, \Countable {
-    /**
-     * @var \Phile\Model\Page[] array of pages found
-     */
-    private $pages;
+	/**
+	 * @var callback A function to be used for loading the pages.
+	 */
+	private	$loader;
 
-    /**
-     * @var array array of search options
-     */
-    private $options;
+	/**
+	 * @var \Phile\Model\Page[] Array of loaded pages.
+	 */
+	private	$pages;
 
-    /**
-     * @var string base search folder
-     */
-    private $folder;
+	public function	__construct($loader){
+		$this->loader =	$loader;
+	}
 
-    /**
-     * @var \Phile\Repository\Page page repository
-     */
-    private $repository;
+	private	function load(){
+		if ($this->pages === null){
+			$this->pages = call_user_func($this->loader);
+		}
+	}
 
-    public function __construct(array $options, $folder, \Phile\Repository\Page $repository){
-        $this->options = $options;
-        $this->folder = $folder;
-        $this->repository = $repository;
-    }
+	public function	getIterator(){
+		$this->load();
+		return new \ArrayIterator($this->pages);
+	}
 
-    public function getIterator(){
-        $this->load();
-        return new \ArrayIterator($this->pages);
-    }
+	public function	offsetExists ($offset){
+		$this->load();
+		return isset($this->pages[$offset]);
+	}
 
-    public function offsetExists ($offset){
-        $this->load();
-        return isset($this->pages[$offset]);
-    }
+	public function	offsetGet($offset){
+		$this->load();
+		return $this->pages[$offset];
+	}
 
-    public function offsetGet($offset){
-        $this->load();
-        return $this->pages[$offset];
-    }
+	public function	offsetSet($offset, $value){
+		$this->load();
+		$this->pages[$offset] =	$value;
+	}
 
-    public function offsetSet($offset, $value){
-        $this->load();
-        $this->pages[$offset] = $value;
-    }
+	public function	offsetUnset($offset){
+		$this->load();
+		unset($this->pages[$offset]);
+	}
 
-    public function offsetUnset($offset){
-        $this->load();
-        unset($this->pages[$offset]);
-    }
-
-    public function count(){
-        $this->load();
-        return count($this->pages);
-    }
-
-    private function load(){
-        if ($this->pages === null){
-            $this->findPages();
-            if (!empty($this->options['pages_order'])){
-                $this->sortPages();
-            }
-        }
-    }
-
-    private function findPages(){
-        // ignore files with a leading '.' in its filename
-        $files = Utility::getFiles($this->folder, '\Phile\FilterIterator\ContentFileFilterIterator');
-        $this->pages = [];
-        $extLen = -strlen(CONTENT_EXT);
-        $folderLen = strlen($this->folder);
-        foreach ($files as $file) {
-            $pageId = substr($file, $folderLen, $extLen);
-            if ($pageId == '404') {
-                // jump to next page if file is the 404 page
-                continue;
-            }
-
-            $this->pages[] = $this->repository->findByPath($pageId, $this->folder);
-        }
-    }
-
-    private function sortPages(){
-        $criteria = $this->getSortCriteria();
-
-        // prepare search criteria for array_multisort
-        foreach ($criteria as $sort) {
-            $key = $sort['key'];
-            $column = array();
-            foreach ($this->pages as $page) {
-                /** @var \Phile\Model\Page $page */
-                $meta = $page->getMeta();
-                if ($sort['type'] === 'page') {
-                    $method = 'get' . ucfirst($key);
-                    $value = $page->$method();
-                } elseif ($sort['type'] === 'meta') {
-                    $value = $meta->get($key);
-                } else {
-                    continue 2; // ignore unhandled search term
-                }
-                $column[] = $value;
-            }
-            $sortHelper[] = $column;
-            $sortHelper[] = constant('SORT_' . strtoupper($sort['order']));
-        }
-
-        $sortHelper[] = &$this->pages;
-
-        call_user_func_array('array_multisort', $sortHelper);
-    }
-
-    private function getSortCriteria(){
-        // parse search criteria
-        $terms = preg_split('/\s+/', $this->options['pages_order'], -1, PREG_SPLIT_NO_EMPTY);
-        $criteria = [];
-        foreach ($terms as $term) {
-            $term = explode('.', $term);
-            if (count($term) > 1) {
-                $type = array_shift($term);
-            } else {
-                $type = null;
-            }
-            $term = explode(':', $term[0]);
-            $criteria[] = array('type' => $type, 'key' => $term[0], 'order' => $term[1]);
-        }
-
-        return $criteria;
-    }
+	public function	count(){
+		$this->load();
+		return count($this->pages);
+	}
 }
