93f1887c2a78698dade508cd240cb51f582ba2df
Schlaefer <openmail+sourcecode@siezi.com>
2015-07-11 09:27:47 +0200

    add test cases for file filters and Utility::getFiles()
    Refs #154 #155 #156

---
+++ tests/Phile/FilterIterator/GeneralFileFilterIteratorTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PhileTest\FilterIterator;
+
+use Phile\FilterIterator\GeneralFileFilterIterator;
+
+/**
+ * class GeneralFileFilterIteratorTest
+ *
+ * @author  Phile CMS
+ * @link    https://philecms.com
+ * @license http://opensource.org/licenses/MIT
+ */
+class GeneralFileFilterIteratorTest extends \PHPUnit_Framework_TestCase {
+
+	public function testGeneralFileFilterIterator() {
+		$folder = PLUGINS_DIR . 'phile/testPlugin/content';
+		$files = new GeneralFileFilterIterator(new \DirectoryIterator($folder));
+		$result = [];
+		foreach ($files as $file) {
+			$result[] = $file->getFilename();
+		}
+		sort($result);
+		$this->assertEquals(['.', '..', 'a.md', 'b.txt', 'sub'], $result);
+	}
+}
