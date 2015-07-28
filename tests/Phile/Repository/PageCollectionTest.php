daa262454d44c49b3d92ed8310e6521cff931d8c
Schlaefer <openmail+sourcecode@siezi.com>
2015-07-28 13:26:14 +0200

    add test cases for PageCollection

---
+++ tests/Phile/Repository/PageCollectionTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use Phile\Repository\PageCollection;
+
+/**
+ * the PageCollectionTest class
+ *
+ * @author  PhileCMS
+ * @link    https://philecms.com
+ * @license http://opensource.org/licenses/MIT
+ */
+class PageCollectionTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var PageCollection;
+	 */
+	protected $collection;
+
+	protected $fixture = [0 => 'a', 1 => 'b'];
+
+	protected function setUp() {
+		$loader = function () {
+			return $this->fixture;
+		};
+		$this->collection = new PageCollection($loader);
+	}
+
+	public function testArrayAccess() {
+		$result = $this->collection;
+		$this->assertEquals($this->fixture[0], $result[0]);
+		$this->assertEquals($this->fixture[1], $result[1]);
+	}
+
+	public function testCount() {
+		$result = $this->collection;
+		$this->assertEquals(count($this->fixture), count($result));
+	}
+
+	public function testTraversable() {
+		$result = $this->collection;
+		foreach ($result as $key => $value) {
+			$this->assertEquals($this->fixture[$key], $value);
+		}
+	}
+}
