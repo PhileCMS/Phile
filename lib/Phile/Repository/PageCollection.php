9761bc693590fee565a5929838cdd2755e485c66
Schlaefer <openmail+sourcecode@siezi.com>
2015-07-28 13:01:26 +0200

    format PageCollection to current Phile .editorconfig rules

--- lib/Phile/Repository/PageCollection.php
+++ lib/Phile/Repository/PageCollection.php
@@ -2,7 +2,6 @@
 
 namespace Phile\Repository;
 
-
 /**
  * Page collection which delays searching for and loading pages until necessary.
  *
@@ -22,42 +21,42 @@ class PageCollection implements \ArrayAccess, \IteratorAggregate, \Countable {
 	 */
 	private	$pages;
 
-	public function	__construct($loader){
-		$this->loader =	$loader;
+	public function __construct($loader) {
+		$this->loader = $loader;
 	}
 
-	private	function load(){
-		if ($this->pages === null){
+	private function load() {
+		if ($this->pages === null) {
 			$this->pages = call_user_func($this->loader);
 		}
 	}
 
-	public function	getIterator(){
+	public function getIterator() {
 		$this->load();
 		return new \ArrayIterator($this->pages);
 	}
 
-	public function	offsetExists ($offset){
+	public function offsetExists($offset) {
 		$this->load();
 		return isset($this->pages[$offset]);
 	}
 
-	public function	offsetGet($offset){
+	public function	offsetGet($offset) {
 		$this->load();
 		return $this->pages[$offset];
 	}
 
-	public function	offsetSet($offset, $value){
+	public function	offsetSet($offset, $value) {
 		$this->load();
 		$this->pages[$offset] =	$value;
 	}
 
-	public function	offsetUnset($offset){
+	public function	offsetUnset($offset) {
 		$this->load();
 		unset($this->pages[$offset]);
 	}
 
-	public function	count(){
+	public function	count() {
 		$this->load();
 		return count($this->pages);
 	}
