606d7ea8725b8268771861a65c669118023c39cd
Schlaefer <openmail+sourcecode@siezi.com>
2015-08-05 09:59:02 +0200

    process @deprecated for 1.6.0 release

--- lib/Phile/Plugin/AbstractPlugin.php
+++ lib/Phile/Plugin/AbstractPlugin.php
@@ -35,7 +35,7 @@ abstract class AbstractPlugin implements EventObserverInterface {
 	 * for the plugin-user
 	 *
 	 * @param string $pluginKey
-	 * @deprecated since 2015-04-24; will be declared 'final'
+	 * @deprecated since 1.5.1 will be declared 'final'
 	 */
 	public function initializePlugin($pluginKey) {
 		/**
@@ -87,7 +87,7 @@ abstract class AbstractPlugin implements EventObserverInterface {
 	 * backwards compatibility to Phile 1.4
 	 *
 	 * @param array $settings
-	 * @deprecated since 2015-04-24; will be removed
+	 * @deprecated since 1.5.1 will be removed
 	 */
 	public function injectSettings(array $settings = null) {
 	}
