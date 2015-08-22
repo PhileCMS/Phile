c5edd753edd150eacb12115e93e8d27f4ce651ae
Schlaefer <openmail+sourcecode@siezi.com>
2015-08-22 15:44:56 +0200

    additional PSR-2 formatting

--- tests/Phile/Core/RouterTest.php
+++ tests/Phile/Core/RouterTest.php
@@ -13,114 +13,129 @@ use Phile\Core\Router;
  * @license http://opensource.org/licenses/MIT
  * @package PhileTest
  */
-class RouterTest extends \PHPUnit_Framework_TestCase {
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
 
-	protected $settings;
+    protected $settings;
 
-	/**
-	 * @var Router
-	 */
-	protected $router;
+    /**
+     * @var Router
+     */
+    protected $router;
 
-	protected function setUp() {
-		$this->router = new Router();
-		$this->settings = Registry::get('Phile_Settings');
-		parent::setup();
-	}
+    protected function setUp()
+    {
+        $this->router = new Router();
+        $this->settings = Registry::get('Phile_Settings');
+        parent::setup();
+    }
 
-	protected function tearDown() {
-		Registry::set('Phile_Settings', $this->settings);
-		unset($this->router, $this->settings);
-	}
+    protected function tearDown()
+    {
+        Registry::set('Phile_Settings', $this->settings);
+        unset($this->router, $this->settings);
+    }
 
-	public function testUrlForPageFull() {
-		$this->mockBaseUrl('http://barbaz');
-		$page = 'index/foo';
-		$expected = 'http://barbaz/index/foo';
-		$result = $this->router->urlForPage($page);
-		$this->assertEquals($expected, $result);
-	}
+    public function testUrlForPageFull()
+    {
+        $this->mockBaseUrl('http://barbaz');
+        $page = 'index/foo';
+        $expected = 'http://barbaz/index/foo';
+        $result = $this->router->urlForPage($page);
+        $this->assertEquals($expected, $result);
+    }
 
-	public function testUrlForPageRelative() {
-		$page = 'index/foo';
-		$expected = 'index/foo';
-		$result = $this->router->urlForPage($page, false);
-		$this->assertEquals($expected, $result);
-	}
+    public function testUrlForPageRelative()
+    {
+        $page = 'index/foo';
+        $expected = 'index/foo';
+        $result = $this->router->urlForPage($page, false);
+        $this->assertEquals($expected, $result);
+    }
 
-	public function testUrl() {
-		$this->mockBaseUrl('http://barbaz');
-		$expected = 'http://barbaz/sub/page';
-		$result = $this->router->url('sub/page');
-		$this->assertEquals($expected, $result);
-	}
+    public function testUrl()
+    {
+        $this->mockBaseUrl('http://barbaz');
+        $expected = 'http://barbaz/sub/page';
+        $result = $this->router->url('sub/page');
+        $this->assertEquals($expected, $result);
+    }
 
-	public function testGetBaseUrl() {
-		$this->mockBaseUrl();
+    public function testGetBaseUrl()
+    {
+        $this->mockBaseUrl();
 
-		$server = ['PHP_SELF' => '/bar/index.php', 'HTTP_HOST' => 'foo'];
-		$router = new Router($server);
-		$this->assertEquals('http://foo/bar', $router->getBaseUrl());
-	}
+        $server = ['PHP_SELF' => '/bar/index.php', 'HTTP_HOST' => 'foo'];
+        $router = new Router($server);
+        $this->assertEquals('http://foo/bar', $router->getBaseUrl());
+    }
 
-	/**
-	 * test baseUrl on `php -S localhost` server
-	 */
-	public function testGetBaseUrlPhpBuildInServer() {
-		$this->mockBaseUrl();
+    /**
+     * test baseUrl on `php -S localhost` server
+     */
+    public function testGetBaseUrlPhpBuildInServer()
+    {
+        $this->mockBaseUrl();
 
-		$server = ['PHP_SELF' => '/foo/index.php', 'HTTP_HOST' => 'host'];
-		$router = new Router($server);
-		$this->assertEquals('http://host/foo', $router->getBaseUrl());
+        $server = ['PHP_SELF' => '/foo/index.php', 'HTTP_HOST' => 'host'];
+        $router = new Router($server);
+        $this->assertEquals('http://host/foo', $router->getBaseUrl());
 
-		$server = ['PHP_SELF' => '/foo/index.php/sub/page', 'HTTP_HOST' => 'host'];
-		$router = new Router($server);
-		$this->assertEquals('http://host/foo', $router->getBaseUrl());
-	}
+        $server = [
+            'PHP_SELF' => '/foo/index.php/sub/page',
+            'HTTP_HOST' => 'host'
+        ];
+        $router = new Router($server);
+        $this->assertEquals('http://host/foo', $router->getBaseUrl());
+    }
 
-	public function testGetBaseUrlPreset() {
-		$this->mockBaseUrl('https://barbaz');
-		$this->assertEquals('https://barbaz', $this->router->getBaseUrl());
-	}
+    public function testGetBaseUrlPreset()
+    {
+        $this->mockBaseUrl('https://barbaz');
+        $this->assertEquals('https://barbaz', $this->router->getBaseUrl());
+    }
 
-	/**
-	 * test that URL is UTF8-decoded
-	 */
-	public function testGetUrlUrlDecoded() {
-		$router = new Router(['REQUEST_URI' => '/bar/b%C3%A4z%20page?q=a']);
-		$this->assertEquals('bar/bäz page', $router->getCurrentUrl());
-	}
+    /**
+     * test that URL is UTF8-decoded
+     */
+    public function testGetUrlUrlDecoded()
+    {
+        $router = new Router(['REQUEST_URI' => '/bar/b%C3%A4z%20page?q=a']);
+        $this->assertEquals('bar/bäz page', $router->getCurrentUrl());
+    }
 
-	/**
-	 * test that base-URL is removed
-	 */
-	public function testGetUrlRemoveUrlPath() {
-		$pathFragment = 'sub';
-		// assume installation in http://localhost/sub
-		$this->mockBaseUrl('http://localhost/' . $pathFragment);
+    /**
+     * test that base-URL is removed
+     */
+    public function testGetUrlRemoveUrlPath()
+    {
+        $pathFragment = 'sub';
+        // assume installation in http://localhost/sub
+        $this->mockBaseUrl('http://localhost/' . $pathFragment);
 
-		// incoming request-URI: /sub/sub/page
-		$requestUri = '/' . $pathFragment . '/' . $pathFragment . '/page';
-		$router = new Router(['REQUEST_URI' => $requestUri]);
-		// requested page: sub/page.md
-		$this->assertEquals('sub/page', $router->getCurrentUrl());
-	}
+        // incoming request-URI: /sub/sub/page
+        $requestUri = '/' . $pathFragment . '/' . $pathFragment . '/page';
+        $router = new Router(['REQUEST_URI' => $requestUri]);
+        // requested page: sub/page.md
+        $this->assertEquals('sub/page', $router->getCurrentUrl());
+    }
 
-	public function testGetProtocol() {
-		$this->assertEquals(null, $this->router->getProtocol());
+    public function testGetProtocol()
+    {
+        $this->assertEquals(null, $this->router->getProtocol());
 
-		$router = new Router(['HTTP_HOST' => 'foo']);
-		$this->assertEquals('http', $router->getProtocol());
+        $router = new Router(['HTTP_HOST' => 'foo']);
+        $this->assertEquals('http', $router->getProtocol());
 
-		$router = new Router(['HTTP_HOST' => 'foo', 'HTTPS' => 'ON']);
-		$this->assertEquals('https', $router->getProtocol());
-	}
+        $router = new Router(['HTTP_HOST' => 'foo', 'HTTPS' => 'ON']);
+        $this->assertEquals('https', $router->getProtocol());
+    }
 
-	public function mockBaseUrl($url = '') {
-		Registry::set(
-			'Phile_Settings',
-			['base_url' => $url] + $this->settings
-		);
-	}
-
+    public function mockBaseUrl($url = '')
+    {
+        Registry::set(
+            'Phile_Settings',
+            ['base_url' => $url] + $this->settings
+        );
+    }
 }
--- tests/Phile/ServiceLocatorTest.php
+++ tests/Phile/ServiceLocatorTest.php
@@ -8,7 +8,6 @@
 
 namespace PhileTest;
 
-
 /**
  * the ServiceLocatorTest class
  *
@@ -17,88 +16,178 @@ namespace PhileTest;
  * @license http://opensource.org/licenses/MIT
  * @package PhileTest
  */
-class ServiceLocatorTest extends \PHPUnit_Framework_TestCase {
+class ServiceLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testServicePhileCacheExists()
+    {
+        $this->assertEquals(
+            false,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Cache'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileCacheExists() {
-		$this->assertEquals(false, \Phile\Core\ServiceLocator::hasService('Phile_Cache'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileTemplateExists()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Template'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileTemplateExists() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Template'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileParserExists()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Parser'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileParserExists() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Parser'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileDatePersistenceExists()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Data_Persistence'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileDatePersistenceExists() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Data_Persistence'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileParserMetaExists()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Parser_Meta'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileParserMetaExists() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Parser_Meta'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileErrorHandlerExists()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_ErrorHandler'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileErrorHandlerExists() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_ErrorHandler'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileTemplateExistsAndHasCorrectInstance()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Template'
+            )
+        );
+        $this->assertInstanceOf(
+            '\Phile\ServiceLocator\TemplateInterface',
+            \Phile\Core\ServiceLocator::getService(
+                'Phile_Template'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileTemplateExistsAndHasCorrectInstance() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Template'));
-		$this->assertInstanceOf('\Phile\ServiceLocator\TemplateInterface', \Phile\Core\ServiceLocator::getService('Phile_Template'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileParserExistsAndHasCorrectInstance()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Parser'
+            )
+        );
+        $this->assertInstanceOf(
+            '\Phile\ServiceLocator\ParserInterface',
+            \Phile\Core\ServiceLocator::getService(
+                'Phile_Parser'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileParserExistsAndHasCorrectInstance() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Parser'));
-		$this->assertInstanceOf('\Phile\ServiceLocator\ParserInterface', \Phile\Core\ServiceLocator::getService('Phile_Parser'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileDatePersistenceExistsAndHasCorrectInstance()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Data_Persistence'
+            )
+        );
+        $this->assertInstanceOf(
+            '\Phile\ServiceLocator\PersistenceInterface',
+            \Phile\Core\ServiceLocator::getService(
+                'Phile_Data_Persistence'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileDatePersistenceExistsAndHasCorrectInstance() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Data_Persistence'));
-		$this->assertInstanceOf('\Phile\ServiceLocator\PersistenceInterface', \Phile\Core\ServiceLocator::getService('Phile_Data_Persistence'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileParserMetaExistsAndHasCorrectInstance()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_Parser_Meta'
+            )
+        );
+        $this->assertInstanceOf(
+            '\Phile\ServiceLocator\MetaInterface',
+            \Phile\Core\ServiceLocator::getService(
+                'Phile_Parser_Meta'
+            )
+        );
+    }
 
-	/**
-	 *
-	 */
-	public function testServicePhileParserMetaExistsAndHasCorrectInstance() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_Parser_Meta'));
-		$this->assertInstanceOf('\Phile\ServiceLocator\MetaInterface', \Phile\Core\ServiceLocator::getService('Phile_Parser_Meta'));
-	}
-
-	/**
-	 *
-	 */
-	public function testServicePhileErrorHandlerExistsAndHasCorrectInstance() {
-		$this->assertEquals(true, \Phile\Core\ServiceLocator::hasService('Phile_ErrorHandler'));
-		$this->assertInstanceOf('\Phile\ServiceLocator\ErrorHandlerInterface', \Phile\Core\ServiceLocator::getService('Phile_ErrorHandler'));
-	}
+    /**
+     *
+     */
+    public function testServicePhileErrorHandlerExistsAndHasCorrectInstance()
+    {
+        $this->assertEquals(
+            true,
+            \Phile\Core\ServiceLocator::hasService(
+                'Phile_ErrorHandler'
+            )
+        );
+        $this->assertInstanceOf(
+            '\Phile\ServiceLocator\ErrorHandlerInterface',
+            \Phile\Core\ServiceLocator::getService(
+                'Phile_ErrorHandler'
+            )
+        );
+    }
 }
- 
