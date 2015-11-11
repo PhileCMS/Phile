e05ea2caa9c4a841dacf02a59a93bb33a5ba2914
Schlaefer <openmail+sourcecode@siezi.com>
2015-11-11 11:22:05 +0100

    make meta-data fences config parameter

--- plugins/phile/parserMeta/config.php
+++ plugins/phile/parserMeta/config.php
@@ -2,4 +2,13 @@
 /**
  * config file
  */
-return array();
+return [
+    /**
+     * open and close tokens for meta tags
+     */
+    'fences' =>
+        [
+            'c' => ['open' => '/*', 'close' => '*/'],
+            'html' => ['open' => '<!--', 'close' => '-->']
+        ]
+];
